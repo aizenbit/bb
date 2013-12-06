ad45e583ee1a0a47d04fe8a5f9a157b9a4890dfe
den5509 <den5509@mail.ru>
2013-12-07 01:19:33 +0400

    Пытаемся написать функции класса GameMechanics. Добавили туда массив картинок и пару фунций. Код пока нерабочий.

--- gamemechanics.h
+++ gamemechanics.h
@@ -1,16 +1,19 @@
-/*
- *  Класс, отвечающий за игровую механику
- */
+
 #ifndef GAMEMECHANICS_H
 #define GAMEMECHANICS_H
 
 #include <QObject>
+#include <QImage>
+#include <QPoint>
 
 class GameMechanics : public QObject
 {
     Q_OBJECT
 
-
+private:
+    int pieceCount;
+    QImage** array;
+    QPoint emptyPeace;
 public:
     QString* imageName;
     GameMechanics(QObject *parent = 0);
@@ -20,6 +23,9 @@ signals:
 public slots:
     void newGame(QString* imageName);
     void hint();
+    void mixArray();
+    void imagePressed();
+
 };
 
 #endif // GAMEMECHANICS_H
