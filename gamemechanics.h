7a0c744cc3b2946d390028d98ac2f39c7b99d133
aizenbit <alextopchiy@gmail.com>
2013-12-07 01:52:52 +0400

    Revert "Откат на коммит до merge"
    This reverts commit 2d996074be3af637483b7de279f5f6fb0461fcaa.

--- gamemechanics.h
+++ gamemechanics.h
@@ -1,19 +1,16 @@
-
+/*
+ *  Класс, отвечающий за игровую механику
+ */
 #ifndef GAMEMECHANICS_H
 #define GAMEMECHANICS_H
 
 #include <QObject>
-#include <QImage>
-#include <QPoint>
 
 class GameMechanics : public QObject
 {
     Q_OBJECT
 
-private:
-    int pieceCount;
-    QImage** array;
-    QPoint emptyPeace;
+
 public:
     QString* imageName;
     GameMechanics(QObject *parent = 0);
@@ -23,9 +20,6 @@ signals:
 public slots:
     void newGame(QString* imageName);
     void hint();
-    void mixArray();
-    void imagePressed();
-
 };
 
 #endif // GAMEMECHANICS_H
