65c2a195f252e07b39e2a03de4a2cb472b3bcb27
den5509 <den5509@mail.ru>
2013-12-17 22:28:04 +0400

    Переписана процедура перемешивания.

--- gamemechanics.cpp
+++ gamemechanics.cpp
@@ -11,59 +11,26 @@ GameMechanics::GameMechanics(QWidget *parent) :
     array = new qwaqwa*[pieceCount];
 	for (int i = 0; i < pieceCount; i++)
         array[i] = new qwaqwa[pieceCount];
+
     for (int x = 0; x<pieceCount;x++)
         for(int y = 0; y<pieceCount;y++)
         {
             array[x][y].x=x;
             array[x][y].y=y;
         }
+
 }
 
 //-----------------------------------------
 
 void GameMechanics::mixArray()
-{ 	//Начинаем перемешивать части картинок по алгоритму
-    int x1,y1,x2,y2;
-    x1=0;
-    y1=0;
-    for(int x=0;x<pieceCount;x++){
-        for(int y=0;y<(pieceCount+1);y++)
-            if(y==pieceCount)
-            {
-                qwaqwa t;
-                t = array[x][y];
-                array[x][y] = array[x+1][y];
-                array[x+1][y] = t;
-            }
-            else
-            {
-                qwaqwa t;
-                t = array[x][y];
-                array[x][y] = array[x][y+1];
-                array[x][y+1] = t;
-            }
-    }
-    for(int k=0;k<(pieceCount*(rand()%(5)));k++){
-       for(y2=0;y2<pieceCount;y2++)
-           for(x2=0;x2<(pieceCount+1);x2++){
-            if(x2==pieceCount)
-            {
-                qwaqwa t;
-                t = array[x2][y2];
-                array[x2][y2] = array[x2][y2+1];
-                array[x2][y2+1] = t;
-            }
-            else
-            {
-                qwaqwa t;
-                t = array[x2][y2];
-                array[x2][y2] = array[x2+1][y2];
-                array[x2+1][y2] = t;
-            }
-        }
-    }
+    //Начинаем перемешивать части картинок по алгоритму
+{int n=rand()%(59);
+         for(int x = 0; x < ((pieceCount*pieceCount-1)*(rand()%(n)));x++)
+             array[x/pieceCount][x%pieceCount].img.swap(array[(x+1)/pieceCount][(x+1)%pieceCount].img);
+         for(int y = 0; y < ((pieceCount*pieceCount-1)*(rand()%(7)));y++)//
+             array[y/pieceCount][y%pieceCount].img.swap(array[(y+1)/pieceCount][(y+1)%pieceCount].img);
 }
-
 
 //-----------------------------------------
 
@@ -86,6 +53,7 @@ void GameMechanics::newGame()
     {
         array[x][pieceCount-1].img = image->copy(pieceWidth*x,pieceHeight*(pieceCount-1),pieceWidth,pieceHeight);
     }
+    mixArray();
 }
 
 //----------------------------------------
@@ -118,15 +86,15 @@ void GameMechanics::paintEvent(QPaintEvent *paintEvent)
     //рисуем картинки
     for(int x = 0; x < pieceCount; x++)
     {
-        for(int y = 0; y < pieceCount-1; y++)
+        for(int y = 0; y < pieceCount; y++)
         {
             painter.drawImage(pieceWidth*x,pieceHeight*y,array[x][y].img);
         }
     }
-    for(int x = 0; x < pieceCount-1; x++)
+    /*for(int x = 0; x < pieceCount-1; x++)
     {
         painter.drawImage(pieceWidth*x,pieceHeight*(pieceCount-1),array[x][pieceCount-1].img);
-    }
+    }*/
     //рисуем линии между картинками
     painter.setPen(QColor(0,0,0));
     for(int x = pieceWidth; x < this->width();x+=pieceWidth)
